5281225e4d10efa8a2fecd05661cb72cb6709183
Igor Wallacy <doks92@gmail.com>
2022-05-19 20:54:04 -0300

    novos icones e tempo de sessao

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,6 +10,8 @@ import { Button } from "primereact/button";
 import Context from "../../contexts";
 import { useNavigate } from "react-router-dom";
 
+import CountdownTimer from "react-component-countdown-timer";
+
 const Header = () => {
   const isLogado = useContext(Context);
 
@@ -38,36 +40,59 @@ const Header = () => {
           alt="img"
         />
 
+        
+
         {isLogado.logado ? (
-          <div className="avatar">
-            <Tooltip target=".tooltip-avatar" autoHide={false} position="bottom" >
-              
+          <>
+            <div className="avatar">
+              <Tooltip
+                target=".tooltip-avatar"
+                autoHide={false}
+                position="bottom"
+              >
                 <Button
                   label="Sair"
                   onClick={() => logout()}
                   type="button"
                   icon="pi pi-times"
-                
                   className="p-button-rounded p-button-danger ml-2"
                 ></Button>
-             
-            </Tooltip>
-            <Avatar
-              icon="pi pi-user"
-              className="tooltip-avatar ml-2 mr-2"
+              </Tooltip>
+
               
-              size="large"
-              style={{
-                backgroundColor: "#f2f2f2",
-                color: "#DC2424",
-                margin: "5px",
-              }}
-              shape="circle"
-            />
-            <h4>
-              {isLogado.usuarioLogado} - {nome}{" "}
-            </h4>
-          </div>
+
+              <Avatar
+                icon="pi pi-user"
+                className="tooltip-avatar ml-2 mr-2"
+                size="large"
+                style={{
+                  backgroundColor: "#f2f2f2",
+                  color: "#DC2424",
+                  margin: "5px",
+                }}
+                shape="circle"
+              />
+              <h4>
+                {isLogado.usuarioLogado} - {nome}{" "}
+              </h4>
+              
+            </div>
+            <div className="expira">
+          <h4>Acesso expira em </h4>{" "}
+          <CountdownTimer
+            border
+            responsive
+            color="#FFF"
+            backgroundColor="#db3236"
+            onEnd={() => document.location.reload(true)}
+            hourTitle="Hora(s)"
+            minuteTitle="Min"
+            secondTitle="Seg"
+            count={JSON.parse(localStorage.getItem("access_token")).expires_in}
+            hideDay
+          />
+        </div>
+          </>
         ) : (
           <></>
         )}
